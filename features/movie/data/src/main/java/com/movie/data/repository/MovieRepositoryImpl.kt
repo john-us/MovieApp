02150140a56fd6cec66c427cc2df0a5858d598dc
shubham <shubham.vishnoi@publicissapient.com>
2023-05-25 14:23:00 +0530

    moved API call on IO thread

--- features/movie/data/src/main/java/com/movie/data/repository/MovieRepositoryImpl.kt
+++ features/movie/data/src/main/java/com/movie/data/repository/MovieRepositoryImpl.kt
@@ -7,6 +7,8 @@ import com.movie.common.network.NetworkException
 import com.movie.common.network.Result
 import com.movie.data.model.MovieDetailModel
 import com.movie.data.model.MovieListModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import okio.IOException
 import javax.inject.Inject
 
@@ -14,34 +16,40 @@ class MovieRepositoryImpl @Inject constructor(
     private val service: MovieAPIService
 ) : MovieRepository {
     override suspend fun getMovieList(): Result<MovieListModel> {
-        try {
-            val response = service.getMovieList()
-            if (response.isSuccessful) {
-                val movieListModel = response.body()
-                return movieListModel?.let { Result.Success(movieListModel) } ?: Result.Error(
-                    ApiException(response.code(), response.message())
-                )
+        return withContext(Dispatchers.IO) {
+            try {
+                val response = service.getMovieList()
+                if (response.isSuccessful) {
+                    val movieListModel = response.body()
+                    return@withContext movieListModel?.let { Result.Success(movieListModel) }
+                        ?: Result.Error(
+                            ApiException(response.code(), response.message())
+                        )
+                }
+            } catch (e: IOException) {
+                return@withContext Result.Error(NetworkException(e.message))
             }
-        } catch (e: IOException) {
-            return Result.Error(NetworkException(e.message))
+            return@withContext Result.Error(DataException(CommonConstant.UnknownError))
         }
-        return Result.Error(DataException(CommonConstant.UnknownError))
 
     }
 
     override suspend fun getMovieDetails(movieId: Long): Result<MovieDetailModel> {
-        try {
-            val response = service.getMovieDetails(movieId)
-            if (response.isSuccessful) {
-                val movieDetailModel = response.body()
-                return movieDetailModel?.let { Result.Success(movieDetailModel) } ?: Result.Error(
-                    ApiException(response.code(), response.message())
-                )
+        return withContext(Dispatchers.IO) {
+            try {
+                val response = service.getMovieDetails(movieId)
+                if (response.isSuccessful) {
+                    val movieDetailModel = response.body()
+                    return@withContext movieDetailModel?.let { Result.Success(movieDetailModel) }
+                        ?: Result.Error(
+                            ApiException(response.code(), response.message())
+                        )
+                }
+            } catch (e: IOException) {
+                return@withContext Result.Error(NetworkException(e.message))
             }
-        } catch (e: IOException) {
-            return Result.Error(NetworkException(e.message))
+            return@withContext Result.Error(DataException(CommonConstant.UnknownError))
         }
-        return Result.Error(DataException(CommonConstant.UnknownError))
     }
 
 }
