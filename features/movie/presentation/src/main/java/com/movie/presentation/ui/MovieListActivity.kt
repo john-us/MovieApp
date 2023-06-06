429beaba395b4b128c8eff9f23c2c6f9eb3914f0
shubham <shubham.vishnoi@publicissapient.com>
2023-06-06 13:31:18 +0530

    removed redundent dependency

--- features/movie/presentation/src/main/java/com/movie/presentation/ui/MovieListActivity.kt
+++ features/movie/presentation/src/main/java/com/movie/presentation/ui/MovieListActivity.kt
@@ -1,14 +1,14 @@
 package com.movie.presentation.ui
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.compose.rememberNavController
 import com.movie.presentation.navigation.NavGraph
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MovieListActivity : AppCompatActivity() {
+class MovieListActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
